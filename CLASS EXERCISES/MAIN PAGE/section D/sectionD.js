d1a92c1b506c76658b26e608747f83826af345dd
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-10-15 15:06:57 +0200

    1 mod + 4 new files

---
+++ CLASS EXERCISES/MAIN PAGE/section D/sectionD.js
@@ -0,0 +1,15 @@
+let grade = 85;
+let attendance = 90;
+if (grade >= 80 && attendance >= 85) {
+  console.log("Scholarship Approved");
+} else {
+  console.log("Not Approved");
+}
+
+let age = 20;
+let citizenship = "SA";
+if (age >= 18 && citizenship === "SA") {
+  console.log("Eligible to vote");
+} else {
+  console.log("Not eligible to vote");
+}
