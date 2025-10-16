a5ffee869289b1a5e2eed78a99c4454d12a92867
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-10-16 11:00:19 +0200

    modded

--- LABS/DIALOG BOX/DialogBox.js
+++ LABS/DIALOG BOX/DialogBox.js
@@ -1,22 +1,32 @@
-let contacts = [{
+let contacts = [
+  {
     name: "Tyler Van Wyk",
-    phone: "(021) 719 6429",
-    email: "TylerVanWyk.WorkShop.10@Yahoo.co.za"
-}, {
+    phone: "(021) 777 7777",
+    email: "tylervanwyk.WorkShop.10@yahoo.co.za",
+  },
+  {
     name: "Raja Nilla",
-    Phone: "021 563 2899",
-    email: "RajaNilla.05@yahoo.co.uk",
-}, {
+    phone: "021 563 2899",
+    email: "rajanilla.05@yahoo.co.uk",
+  },
+  {
     name: "Helen Richards",
-    phone: "0800 288 928",
-    email: "HellenRich.06@yahoo.co.us",
-}];
+    phone: "080 288 9280",
+    email: "hellenrich.06@yahoo.co.us",
+  },
+];
 contacts.push({
-    name: prompt ("Enter user name"),
-    phone: prompt ("Enter user phone"),
-    email: prompt ("Enter user email"),
+  name: prompt("Enter user name"),
+  phone: prompt("Enter user phone"),
+  email: prompt("Enter user email"),
 });
 let last = contacts.length - 1;
 
-console.log(`${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`);
-console.log(`${contacts[last].name} / ${contacts[last].phone} / ${conacts[last].email.email}`);
+console.log(
+  `${contacts[0].name} / ${contacts[0].phone} / ${contacts[0].email}`
+);
+console.log(
+  `${contacts[last].name} / ${contacts[last].phone} / ${conacts[last].email.email}`
+);
+
+
